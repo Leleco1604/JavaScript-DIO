6934061ea84089c897f2a1cd2e524cf9ad514eb3
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-08-07 19:41:07 -0300

    lista

--- listas(arrays)/aula.js
+++ listas(arrays)/aula.js
@@ -1,35 +1,42 @@
 
-// const alunos = ['Leonardo', 'Davi', 'Ana vitoria'];
+const alunos = ['Leonardo', 'Davi', 'Ana vitoria'];
 
 
 // // adicionando a lista
-// alunos.push('Renan');
+alunos.push('Renan');
 
-// alunos[4] = 'Leonardo';
+alunos[4] = 'Leonardo';
 
 // // removendo da lista
-// alunos.pop
+alunos.pop
 
-// //  console.log(alunos[0]);
-// //  console.log(alunos[1]);
-// //  console.log(alunos[2]);
-//  console.log(alunos);
+ console.log(alunos[0]);
+ console.log(alunos[1]);
+ console.log(alunos[2]);
+ console.log(alunos);
 
 // tamanho as notas
+alunos.length();
 
-// const notas =  [];
 
-// notas.push(5);
-// notas.push(7);
-// notas.push(8);
-// notas.push(2);
-// notas.push(5);
+const notas =  [];
 
-// const soma = notas[0] +  notas[1] +  notas[2] +  notas[3] +  notas[4];
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
 
-// console.log( soma / notas.length);
+const soma = notas[0] +  notas[1] +  notas[2] +  notas[3] +  notas[4];
+console.log( soma / notas.length);
 
-for (let i = 0; i < 10; i++) {
 
+
+//Explicando o For
+const nome = 'Leonardo Gabriel Barbosa de Melo';
+
+
+for (let i = 0; i <nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);
 }
-
