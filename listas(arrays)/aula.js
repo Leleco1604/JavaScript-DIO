892ebf4e34f57037c69e1529802ac38280cfebd6
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-08-07 17:57:37 -0300

    explicação de arrays

---
+++ listas(arrays)/aula.js
@@ -0,0 +1,35 @@
+
+// const alunos = ['Leonardo', 'Davi', 'Ana vitoria'];
+
+
+// // adicionando a lista
+// alunos.push('Renan');
+
+// alunos[4] = 'Leonardo';
+
+// // removendo da lista
+// alunos.pop
+
+// //  console.log(alunos[0]);
+// //  console.log(alunos[1]);
+// //  console.log(alunos[2]);
+//  console.log(alunos);
+
+// tamanho as notas
+
+// const notas =  [];
+
+// notas.push(5);
+// notas.push(7);
+// notas.push(8);
+// notas.push(2);
+// notas.push(5);
+
+// const soma = notas[0] +  notas[1] +  notas[2] +  notas[3] +  notas[4];
+
+// console.log( soma / notas.length);
+
+for (let i = 0; i < 10; i++) {
+
+}
+
